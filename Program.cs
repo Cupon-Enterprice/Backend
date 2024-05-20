d5ab33d6f7f5f82dfc6ad6bd4dcf260be0d95d04
duvian <duvianmartin@gmail.com>
2024-05-20 17:09:32 -0500

    se crea inyeccion de base de datos y se avanza en el codigo de services

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+
+builder.Services.AddDbContext<DataContext>(options =>
+options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnetion"),
+Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.20-mysql")));
+
 
 var app = builder.Build();
+
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Services/ICuponesRepository.cs
+++ Services/ICuponesRepository.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Models;
 
 namespace Backend.Services
 {
     public interface ICuponesRepository
     {
-        
+        IEnumerable<Cupon> ListarCupones();
+        Cupon DetallesCupon(int Id);
+        void CrearCupon(Cupon cupon);
+        void EliminarCupon(int Id);
+        void ActualizarCupon(int Id,Cupon cupon);
     }
 }
