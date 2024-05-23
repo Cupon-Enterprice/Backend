751053c3cd2a2a3dbefb9006178ab54711419b4b
DeklanBeckett <juandausuga16@gmail.com>
2024-05-23 00:25:26 -0500

    se soluciona la genereacion del codigodel cupon

--- Services/CuponServices.cs
+++ Services/CuponServices.cs
@@ -4,15 +4,15 @@ namespace Backend.Services
 {
     public class CuponService
     {
-        private static readonly char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
         private static readonly Random random = new Random();
+        private const string caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
         public string GenerateRandomCuponCode(int length = 8)
         {
             char[] result = new char[length];
             for (int i = 0; i < length; i++)
             {
-                result[i] = chars[random.Next(chars.Length)];
+                result[i] = caracteres[random.Next(caracteres.Length)];
             }
 
             return new string(result);
