cadb1a9da861298bb3cdca63f8ef37cd92bc31d0
Mariana <marianabedoyac01@gmail.com>
2024-05-23 18:34:40 -0500

    Se configuro el servicio de cupones en la funcion eliminar

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -4,7 +4,7 @@ namespace Backend.Models
         public int? Id { get; set; }
         public string? Nombre { get; set; }
         public string? Correo { get; set; }
-        public string? Contraseña {get; set; }
+        public string? Clave {get; set; }
 
         public ICollection<Cupon>? Cupones { get; }
     }
