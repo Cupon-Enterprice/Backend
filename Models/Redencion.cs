b0181c2ce13f2f40f980fe202edaf904d52de5b0
Mariana <marianabedoyac01@gmail.com>
2024-05-27 19:20:56 -0500

    Redencion y validacion

--- Models/Redencion.cs
+++ Models/Redencion.cs
@@ -2,11 +2,10 @@ namespace Backend.Models{
     public class Redencion{
         public int? Id { get; set;}
         public DateTime? FechaRendencion{get; set;} = DateTime.Now;
-        public int? CuponesId {get; set;} = 1;
-        public int? UsuariosId {get; set;} =1;
+        public int? CuponesId {get; set;}
+        public int? UsuariosId {get; set;}
 
         public Cupon Cupones {get;}
-        public Usuario Usuarios {get;}
         
     }
 }
---
+++ Models/ReedemRequest.cs
@@ -0,0 +1,8 @@
+namespace Backend.Models
+{
+    public class ReedemRequest{
+        public int UsuariosId { get; set; }
+        public string? CodigoCupon { get; set; }
+    }
+
+}
